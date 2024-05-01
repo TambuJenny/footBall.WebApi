62b4e5071d72072f93da30e533ceaa400da4826b
tambujenny <tambujenny@gmail.com>
2024-05-02 00:16:20 +0100

    my first commit

---
+++ src/main/java/androidproject/api/androidprojectapi/AndroidprojectapiApplication.java
@@ -0,0 +1,16 @@
+package androidproject.api.androidprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+
+@SpringBootApplication
+public class AndroidprojectapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroidprojectapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/androidproject/api/androidprojectapi/AndroidprojectapiApplicationTests.java
@@ -0,0 +1,13 @@
+package androidproject.api.androidprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidprojectapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
